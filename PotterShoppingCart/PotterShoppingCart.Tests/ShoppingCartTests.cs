5fbb875d409014e10f0a7b31e6fc6062529d672c
shihjie shyu <shih_jie@outlook.com>
2017-06-06 23:05:57 +0800

    第一次綠燈 還沒寫折扣的部分 只有一本一百相加

--- PotterShoppingCart/PotterShoppingCart.Tests/ShoppingCartTests.cs
+++ PotterShoppingCart/PotterShoppingCart.Tests/ShoppingCartTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotterShoppingCart;
 
 namespace PotterShoppingCart.Tests
 {
@@ -10,7 +11,8 @@ namespace PotterShoppingCart.Tests
     public class ShoppingCartTests
     {
         [TestMethod()]
-        public void Sample1_1stx1_100() {
+        public void CalculatePriceTest_Sample1_1stx1_100()
+        {
             var shoppingCart = new ShoppingCart();
             shoppingCart.Add("Harry Potter 1", 1);
             shoppingCart.Add("Harry Potter 2", 0);
@@ -23,24 +25,6 @@ namespace PotterShoppingCart.Tests
             var actual = shoppingCart.CalculatePrice();
 
             Assert.AreEqual(expected, actual);
-        }
-    }
-    
-    public class ShoppingCart
-    {
-        public ShoppingCart()
-        {
-
-        }
-
-        public void Add(string bookName, int count)
-        {
-
-        }
-
-        public int CalculatePrice()
-        {
-            return 0;
         }
     }
 }
---
+++ PotterShoppingCart/PotterShoppingCart/ShoppingCart.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotterShoppingCart
+{
+    public class ShoppingCart
+    {
+        List<Product> productsOfCart;
+        BookPrice bookPrice = new BookPrice();
+        public ShoppingCart()
+        {
+            productsOfCart = new List<Product>();
+        }
+
+        public void Add(string bookName, int amouint)
+        {
+            productsOfCart.Add(new Product(bookName, bookPrice.GetPriceByBookname(bookName), amouint));
+        }
+
+        public int CalculatePrice()
+        {
+            int temp = 0;
+            foreach (var product in productsOfCart)
+            {
+                temp += product.Amouint * product.Price;
+            }
+            return temp;
+        }
+    }
+
+    internal class Product
+    {
+        public int Amouint { get; private set; }
+        public string Name { get; private set; }
+        public int Price { get; private set; }
+
+        public Product(string Name, int Price, int Amouint)
+        {
+            this.Name = Name;
+            this.Price = Price;
+            this.Amouint = Amouint;
+        }
+    }
+    public class BookPrice
+    {
+        private Dictionary<string, int> BookPriceDictionary;
+
+        public BookPrice()
+        {
+            BookPriceDictionary = new Dictionary<string, int>();
+            BookPriceDictionary.Add("Harry Potter 1", 100);
+            BookPriceDictionary.Add("Harry Potter 2", 100);
+            BookPriceDictionary.Add("Harry Potter 3", 100);
+            BookPriceDictionary.Add("Harry Potter 4", 100);
+            BookPriceDictionary.Add("Harry Potter 5", 100);
+        }
+        public int GetPriceByBookname(String name)
+        {
+            if (true == (BookPriceDictionary.ContainsKey(name)))
+            {
+                return BookPriceDictionary[name];
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+    }
+}
