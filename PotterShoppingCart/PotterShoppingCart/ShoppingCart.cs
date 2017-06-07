415123a12a728f7da9adaac4457654a0d73b32f2
shihjie shyu <shih_jie@outlook.com>
2017-06-07 10:16:13 +0800

    Product拆分

--- PotterShoppingCart/PotterShoppingCart/ShoppingCart.cs
+++ PotterShoppingCart/PotterShoppingCart/ShoppingCart.cs
@@ -65,18 +65,4 @@ namespace PotterShoppingCart
             }
         }
     }
-
-    public class Product
-    {
-        public int Amount { get; set; }
-        public string Name { get; set; }
-        public int Price { get; set; }
-
-        public Product(string Name, int Price, int Amount)
-        {
-            this.Name = Name;
-            this.Price = Price;
-            this.Amount = Amount;
-        }
-    }
 }
